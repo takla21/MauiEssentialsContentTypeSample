c511598d742d063a406ccca34cbe2c5424bd34f4
Kevin Takla <kevin.takla@nventive.com>
2023-06-09 16:58:22 -0400

    first commit

---
+++ MediaPickerContentType/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MediaPickerContentType;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MediaPickerContentType/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MediaPickerContentType;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		var photo = await Dispatcher.DispatchAsync(async () => await MediaPicker.PickPhotoAsync());
+
+        ContentTypeLabel.Text = photo.ContentType;
+    }
+}
+
